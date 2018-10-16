c35faf51dfbc4fc31de324d8658cf0b10c5d4ade
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-16 13:37:03 +0800

    Converted Gson POJO to Moshi POJO
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/chikeandroid/retrofittutorial2/data/model/Post.java
+++ app/src/main/java/com/chikeandroid/retrofittutorial2/data/model/Post.java
@@ -1,95 +1,46 @@
 package com.chikeandroid.retrofittutorial2.data.model;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.squareup.moshi.Json;
 
-/**
- * Created by Chike on 12/10/2016.
- */
 
 public class Post {
-
-    @SerializedName("title")
-    @Expose
+    @Json(name = "title")
     private String title;
-    @SerializedName("body")
-    @Expose
+    @Json(name = "body")
     private String body;
-    @SerializedName("userId")
-    @Expose
+    @Json(name = "userId")
     private Integer userId;
-    @SerializedName("id")
-    @Expose
+    @Json(name = "id")
     private Integer id;
 
-    /**
-     *
-     * @return
-     * The title
-     */
     public String getTitle() {
         return title;
     }
 
-    /**
-     *
-     * @param title
-     * The title
-     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    /**
-     *
-     * @return
-     * The body
-     */
     public String getBody() {
         return body;
     }
 
-    /**
-     *
-     * @param body
-     * The body
-     */
     public void setBody(String body) {
         this.body = body;
     }
 
-    /**
-     *
-     * @return
-     * The userId
-     */
     public Integer getUserId() {
         return userId;
     }
 
-    /**
-     *
-     * @param userId
-     * The userId
-     */
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
-    /**
-     *
-     * @return
-     * The id
-     */
     public Integer getId() {
         return id;
     }
 
-    /**
-     *
-     * @param id
-     * The id
-     */
     public void setId(Integer id) {
         this.id = id;
     }
